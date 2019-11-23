97964bc1001bafd5a39eadac2c6921d1dfbd20dd
xiaohan <569301464@qq.com>
2019-11-23 17:40:29 +0800

    init

---
+++ TRAlertController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TRAlertController
+//
+//  Created by tion126 on 2019/11/15.
+//  Copyright © 2019 tion126. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
